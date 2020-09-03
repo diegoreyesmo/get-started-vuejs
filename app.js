5e1ae30fa1c25396adc62c5b29998e97108f9877
Diego Reyes <diego.reyes.ext@sodexo.com>
2020-09-03 12:58:21 -0400

    v-onclick

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      joke: ''
+    },
+    methods: {
+        getJoke: function () {
+            this.joke = "some joke";
+      }
     }
 })
