eae4b37d46fef19fd28187527d162c3747bd79d6
Diego Reyes <diego.reyes.ext@sodexo.com>
2020-09-03 16:27:10 -0400

    se agrega vuetify

--- src/main.js
+++ src/main.js
@@ -1,26 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-  data: {
-    message: 'Hello Vue!',
-    joke: ''
-  },
-  methods: {
-      getJoke: function () {
-          let fetchData = async () => {
-              let response = await fetch('http://api.icndb.com/jokes/random');
-              if (response.ok) { 
-                  let {value} = await response.json();
-                  this.joke = value.joke;
-              } else {
-                  alert("HTTP-Error: " + response.status);
-              }
-          }
-          fetchData();
-      }
-  }
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
