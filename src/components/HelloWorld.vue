ea04d53a17c2f84b99681ca73cb38c34fc5b1379
Diego Reyes <diego.reyes.ext@sodexo.com>
2020-09-03 17:14:18 -0400

    fetch categories

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,82 +1,66 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col><v-btn color="primary" v-on:click="getJoke" >Cuéntame un chiste</v-btn></v-col>
-      <v-col><p>{{ randomJoke }}</p></v-col>
+      <v-col>
+        <v-btn color="primary" v-on:click="getJoke">Cuéntame un chiste</v-btn>
+      </v-col>
+      <v-col>
+        <p>{{ randomJoke }}</p>
+      </v-col>
+    </v-row>
+    <v-row class="text-center">
+      <v-col>
+        <v-overflow-btn
+          class="my-2"
+          :items="this.categorias"
+          label="Categoría"
+          target="#dropdown-example-1"
+        ></v-overflow-btn>
+      </v-col>
+      <v-col>
+        <input v-model.number="cantidad" type="number">
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
+export default {
+  name: "HelloWorld",
 
-    data: () => ({
-      randomJoke: '',
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
-    }),
-    methods: {
-      getJoke: function () {
-          let fetchData = async () => {
-              let response = await fetch('http://api.icndb.com/jokes/random');
-              if (response.ok) { 
-                  let {value} = await response.json();
-                  this.randomJoke = value.joke;
-              } else {
-                  alert("HTTP-Error: " + response.status);
-              }
-          }
-          fetchData();
-      }
-    }
-}
+  data: () => ({
+    randomJoke: "",
+    categorias: [],
+    cantidad: 1
+  }),
+  created() {
+    this.obtenerCategorias();
+  },
+  methods: {
+    getJoke: function () {
+      let fetchData = async () => {
+        let response = await fetch("http://api.icndb.com/jokes/random");
+        if (response.ok) {
+          let { value } = await response.json();
+          this.randomJoke = value.joke;
+        } else {
+          alert("HTTP-Error: " + response.status);
+        }
+      };
+      fetchData();
+    },
+    obtenerCategorias: function () {
+      let fetchData = async () => {
+        let response = await fetch("http://api.icndb.com/categories");
+        if (response.ok) {
+          let { value } = await response.json();
+          this.categorias = value;
+        } else {
+          alert("HTTP-Error: " + response.status);
+        }
+      };
+      fetchData();
+    },
+  },
+};
 </script>
